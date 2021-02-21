3db67024ebf9e36022e91ddc201ea46077707140
bhoirvarun6@gmail.com <bhoirvarun6@gmail.com>
2021-02-21 21:31:46 +0530

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by varun bhoir on 21/02/21.
+//  Copyright © 2021 varun bhoir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
