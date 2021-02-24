21ca768d94a68cdecd4cebcf90c8919c88c36510
bhoirvarun6@gmail.com <bhoirvarun6@gmail.com>
2021-02-24 18:20:20 +0530

    manually publishing observableObject changes with objectWillChange - HackingWithSwift

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,61 +8,27 @@
 
 import SwiftUI
 
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
-}
-
-struct ContentView: View {
-    @State private var message = "Hello world"
-    
-    var body: some View {
-        Text(message)
-            .onAppear {
-                self.fetchData(from: "https://www.apple.com") { result in
-                    switch result {
-                    case .success(let str):
-                        print(str)
-                        self.message = "success"
-                    case .failure(let error):
-                        switch error {
-                        case .badURL:
-                            print("Invalid url")
-                            self.message = "Invalid url"
-                        case .requestFailed:
-                            print("Network problems")
-                            self.message = "Network problems"
-                        case .unknown:
-                            print("Unknown")
-                            self.message = "Unknown"
-                        }
-                    }
-                }
+class DelayedUpdater: ObservableObject {
+    var count = 0 {
+        willSet {
+            objectWillChange.send()
         }
     }
     
-    
-    
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        // check for the url is good or not else bad url
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                if let data = data {
-                    // convertu that data into string to use in closure
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    // an error is occured
-                    completion(.failure(.requestFailed))
-                } else {
-                    // it is not possible but still
-                    completion(.failure(.unknown))
-                }
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.count += 1
             }
-        }.resume()
+        }
+    }
+}
+
+
+struct ContentView: View {
+    @ObservedObject var updater = DelayedUpdater()
+    var body: some View {
+        Text("\(updater.count)")
     }
 }
 
